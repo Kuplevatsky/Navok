7a7f815e23d8bf452d192f9524bb916ed8cfa82d
Igor Sidorenko <isidorenko@ebay.com>
2012-02-03 00:03:51 +0200

    Added files for module

---
+++ app/code/local/Navoq/Backup/Helper/Data.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Navoq
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE_AFL.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@navoq.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Navoq Disqus to newer
+ * versions in the future.
+ *
+ * @category    Navoq
+ * @package     Navoq_Backup
+ * @copyright   Copyright (c) 2012 Navoq (http://www.navoq.com)
+ * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+class Navoq_Backup_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
